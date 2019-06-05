2307152fe03a72b059788120e9dbafb2c95f2985
tomocy <tomocy.dev@gmail.com>
2019-06-05 19:24:01 +0900

    use os arg as root path

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/tomocy/server/config"
 	"github.com/tomocy/server/server"
 )
 
 func main() {
+	root, err := parseRoot()
+	if err != nil {
+		showHelp()
+		return
+	}
 	config.Must(config.LoadConfig("./config.yml"))
-	server := server.New("./public")
+	server := server.New(root)
 	server.ListenAndServe(config.Current.Addr)
+}
+
+func parseRoot() (string, error) {
+	if len(os.Args) < 2 {
+		return "", errors.New("root path is not specified")
+	}
+
+	return os.Args[1], nil
+}
+
+func showHelp() {
+	fmt.Print("Usage of ./server:\n\t./server ROOT\n\n")
 }
